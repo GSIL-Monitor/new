72778bbee27237dd1e8be7e2c961e156b6875f90
Lee <624061283@qq.com>
2018-07-13 18:08:38 +0800

    no message

--- pages/device/device.js
+++ pages/device/device.js
@@ -1,28 +1,27 @@
-// pages/deviceManagement/deviceManagement.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     permissions:{
-      Device:3,
-      Ads:1,
-      Devices:2
+      Ads:{name:'广告',permit:false,self:'Ads'},
+      Devices: { name: '设备', permit: false,self:'Devices' }
     },
     get nowTab(){
-      for (var i of [1,2,3]){
-        console.log(i)
+      for (var i in this.permissions){
+        if (this.permissions[i]) return i
       }
-      return this.MaxResultCount_d
     },
-    Ads:false,
-    Devices:false,
+    //设备
     deviceList: [],
     totalCount_d: '',
     page_d: 0,
     MaxResultCount_d: 12
+    //广告
   },
+  changeTab(e){
+    this.setData({
+      nowTab: e.currentTarget.dataset.tab
+    })
+  },
+  //设备
   getDeviceList(cb) {
     getApp().promise(getApp().req)({
       url: '/s/api/services/app/Device/GetDevices',
@@ -33,7 +32,6 @@ Page({
         SkipCount: this.data.page_d * this.data.MaxResultCount_d
       }
     }).then(res => {
-      console.log(res);
       this.setData({
         deviceList: this.data.deviceList.concat(res.items),
         totalCount_d: res.totalCount
@@ -48,7 +46,21 @@ Page({
       url: '/pages/device/deviceDetail/deviceDetail',
     })
   },
-
+  //广告
+  getAdList(cb) {
+    getApp().promise(getApp().req)({
+      url: '/s/api/services/app/Ad/GetAds',
+      data: {
+        // Status: 0,
+        // Sorting: 'name',
+        // MaxResultCount: this.data.MaxResultCount_d,
+        // SkipCount: this.data.page_d * this.data.MaxResultCount_d
+      }
+    }).then(res => {
+      console.log(res);
+      wx.stopPullDownRefresh();
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -60,8 +72,8 @@ Page({
     }).then(res => {
       console.log(res)
       this.setData({
-        Ads: res.auth.grantedPermissions['Pages.Tenant.Ads']=="true"?true:false,
-        Devices: res.auth.grantedPermissions['Pages.Tenant.Devices'] == "true" ? true : false
+        'permissions.Ads.permit': res.auth.grantedPermissions['Pages.Tenant.Ads']=="true"?true:false,
+        'permissions.Devices.permit': res.auth.grantedPermissions['Pages.Tenant.Devices'] == "true" ? true : false
       })
     })
   },
@@ -98,7 +110,6 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-    // console.log('pulldown')
     this.setData({
       totalCount_d: '',
       deviceList: [],
