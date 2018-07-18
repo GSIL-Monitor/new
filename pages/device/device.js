c16111f65d73e583c582a4682703fd7458fec55f
Lee <624061283@qq.com>
2018-07-18 17:00:40 +0800

    no message

--- pages/device/device.js
+++ pages/device/device.js
@@ -15,6 +15,11 @@ Page({
         name: '红包',
         permit: false,
         self: 'Coupons'
+      },
+      Products: {
+        name: '商品',
+        permit: false,
+        self: 'Products'
       }
     },
     get nowTab() {
@@ -22,6 +27,7 @@ Page({
         if (this.permissions[i]) return i
       }
     },
+    stopReachBottom: false,
     //设备
     deviceList: [],
     totalCount_d: '',
@@ -36,7 +42,12 @@ Page({
     couponList: [],
     totalCount_c: '',
     page_c: 0,
-    MaxResultCount_c: 12
+    MaxResultCount_c: 12,
+    //商品
+    productList: [],
+    totalCount_p: '',
+    page_p: 0,
+    MaxResultCount_p: 12
   },
   changeTab(e) {
     this.setData({
@@ -44,13 +55,19 @@ Page({
     })
     this.goGetList(this.data.nowTab);
   },
-  goGetList(e) {//第一次获取数据
+  goGetList(e) { //第一次获取数据
     switch (e) {
       case 'Devices':
         if (!this.data.totalCount_d) this.getDeviceList();
         break;
       case 'Ads':
         if (!this.data.totalCount_a) this.getAdList();
+        break;
+      case 'Coupons':
+        if (!this.data.totalCount_c) this.getCouponList();
+        break;
+      case 'Products':
+        if (!this.data.totalCount_p) this.getProductList();
         break;
       default:
         console.log('没有任何权限,请联系管理员')
@@ -72,6 +89,9 @@ Page({
         totalCount_d: res.totalCount
       })
       wx.stopPullDownRefresh();
+      this.setData({
+        stopReachBottom: false
+      })
     })
   },
   goDevice(e) {
@@ -98,10 +118,63 @@ Page({
         totalCount_a: res.totalCount
       })
       wx.stopPullDownRefresh();
+      this.setData({
+        stopReachBottom: false
+      })
     })
   },
   goAd(e) {
     console.log(e);
+  },
+  //红包
+  getCouponList(cb) {
+    getApp().promise(getApp().req)({
+      url: '/s/api/services/app/Coupon/GetCoupons',
+      data: {
+        // Status: 0,
+        // Sorting: 'name',
+        MaxResultCount: this.data.MaxResultCount_c,
+        SkipCount: this.data.page_c * this.data.MaxResultCount_c
+      }
+    }).then(res => {
+      console.log(res);
+      this.setData({
+        couponList: this.data.couponList.concat(res.items),
+        totalCount_c: res.totalCount
+      })
+      wx.stopPullDownRefresh();
+      this.setData({
+        stopReachBottom: false
+      })
+    })
+  },
+  goCoupon(e) {
+    console.log(e);
+  },
+  //商品
+  getProductList(cb) {
+    getApp().promise(getApp().req)({
+      url: '/s/api/services/app/Product/GetProducts',
+      data: {
+        // Status: 0,
+        // Sorting: 'name',
+        MaxResultCount: this.data.MaxResultCount_p,
+        SkipCount: this.data.page_p * this.data.MaxResultCount_p
+      }
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        productList: this.data.productList.concat(res.items),
+        totalCount_p: res.totalCount
+      })
+      wx.stopPullDownRefresh();
+      this.setData({
+        stopReachBottom: false
+      })
+    })
+  },
+  goProduct(e) {
+    console.log(e)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -115,6 +188,7 @@ Page({
         'permissions.Ads.permit': res.auth.grantedPermissions['Pages.Tenant.Ads'] == "true" ? true : false,
         'permissions.Devices.permit': res.auth.grantedPermissions['Pages.Tenant.Devices'] == "true" ? true : false,
         'permissions.Coupons.permit': res.auth.grantedPermissions['Pages.Tenant.Coupons'] == "true" ? true : false,
+        'permissions.Products.permit': res.auth.grantedPermissions['Pages.Tenant.Products'] == "true" ? true : false,
       })
       this.goGetList(this.data.nowTab);
     })
@@ -153,12 +227,19 @@ Page({
    */
   onPullDownRefresh: function() {
     this.setData({
+      stopReachBottom:false,
       totalCount_d: '',
       deviceList: [],
       page_d: 0,
       totalCount_a: '',
       adList: [],
-      page_a: 0
+      page_a: 0,
+      totalCount_c: '',
+      couponList: [],
+      page_c: 0,
+      totalCount_p: '',
+      productList: [],
+      page_p: 0
     })
     this.goGetList(this.data.nowTab);
   },
@@ -167,17 +248,45 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
+    if (this.data.stopReachBottom) return
+    console.log(this.data.nowTab)
     switch (this.data.nowTab) {
       case 'Devices':
         if (this.data.deviceList.length < this.data.totalCount_d) {
+          this.setData({
+            stopReachBottom: true
+          })
           this.data.page_d++;
           this.getDeviceList();
         }
+        break;
       case 'Ads':
         if (this.data.adList.length < this.data.totalCount_a) {
+          this.setData({
+            stopReachBottom: true
+          })
           this.data.page_a++;
           this.getAdList();
         }
+        break;
+      case 'Coupons':
+        if (this.data.couponList.length < this.data.totalCount_c) {
+          this.setData({
+            stopReachBottom: true
+          })
+          this.data.page_c++;
+          this.getCouponList();
+        }
+        break;
+      case 'Products':
+        if (this.data.productList.length < this.data.totalCount_p) {
+          this.setData({
+            stopReachBottom: true
+          })
+          this.data.page_p++;
+          this.getProductList();
+        }
+        break;
     }
   },
 
